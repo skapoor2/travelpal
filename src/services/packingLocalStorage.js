d650d30cd609854e905ccd748013a9fa5d9b91ee
skapoor2 <skapoor2@uci.edu>
2020-12-10 17:38:57 -0800

    Setting up storage

---
+++ src/services/packingLocalStorage.js
@@ -0,0 +1,26 @@
+// @flow
+import { useState } from "react"
+
+// eslint-disable-next-line flowtype/no-weak-types
+const packingLocalStorage = (key: string, initialValue: any) => {
+  // get value from local storage by key
+  const item = window.localStorage.getItem(key)
+
+  const [storedValue, setStoredValue] = useState(item ? JSON.parse(item) : initialValue)
+
+  // eslint-disable-next-line flowtype/no-weak-types
+  const setValue = (value: any) => {
+    // save state
+    setStoredValue(value)
+    // save to local storage
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  const removeValue = () => {
+    window.localStorage.removeItem(key)
+  }
+
+  return [storedValue, setValue, removeValue]
+}
+
+export default packingLocalStorage
