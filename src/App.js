37a42298e626eb819b3cdb59662f71d1a1ab511b
trishaprile <trishaprilee@gmail.com>
2020-12-10 16:40:33 -0800

    Update navbar styling

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ import BudgetTracker from "./Components/BudgetTracker";
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="App">
         <Router>
-          <Navbar bg="light" expand="lg" sticky="top">
-            <Navbar.Brand href="/">Travel Pal</Navbar.Brand>
+          <Navbar className="navbar" expand="lg" sticky="top">
+            <Navbar.Brand className="nav-brand" href="/">Travel Pal</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/calendar">Calendar</Nav.Link>
-                <Nav.Link href="/packing">Packing List</Nav.Link>
-                <Nav.Link href="/itinerary">Itinerary</Nav.Link>
-                <Nav.Link href="/budget">Budget Tracker</Nav.Link>
+                <Nav.Link className="nav-link" href="/">Home</Nav.Link>
+                <Nav.Link className="nav-link" href="/calendar">Calendar</Nav.Link>
+                <Nav.Link className="nav-link" href="/packing">Packing List</Nav.Link>
+                <Nav.Link className="nav-link" href="/itinerary">Itinerary</Nav.Link>
+                <Nav.Link className="nav-link" href="/budget">Budget Tracker</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
