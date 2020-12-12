59c3e86c5af5ec1a3c36d6b54acaa6cc6117918f
Bryan Snyder <btsnyder@uci.edu>
2020-12-11 21:50:24 -0800

    Imported FullCalendar API into claendar component

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import './calendar.css';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 class Calendar extends Component {
+
+
+
+
+
   render() {
-    return (
-      <div className="calendar">
-        <p>Calendar content</p>
-      </div>
-    )
+    return <FullCalendar 
+              defaultView="dayGridMonth" 
+              plugins={[dayGridPlugin, interactionPlugin]}
+              editable={true}
+              eventDrop={this.handleEventDrop}
+              eventClick={this.handleEventClick}
+              //events={this.formatEvents()}
+          />
   }
 }
+
+
+
+
+
 export default Calendar;
