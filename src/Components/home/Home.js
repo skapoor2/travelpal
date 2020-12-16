d84aa8c985c379eb4855afdc2e260714a50af4f7
trishaprile <trishaprilee@gmail.com>
2020-12-15 23:56:42 -0800

    Update home page

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,50 +1,43 @@
 import React, { Component } from 'react';
 import './home.css';
-import { Card, CardDeck } from 'react-bootstrap';
+import { Card, CardDeck, Container } from 'react-bootstrap';
 import picture from './img/yosemite.jpg';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        <h1>My Trips</h1>
-        <h2>Upcoming Trips</h2>
-        <div className="row">
+        <h1>My Trip</h1>
+        <p>input form for trip information</p>
+        <p>upload image file</p>
+        <Container className="cards">
           <CardDeck>
             <Card className="card">
               <Card.Img className="card-img" variant="top" src={picture} />
               <Card.Body>
-                <Card.Title className="card-title">Yosemite</Card.Title>
-                <Card.Text className="card-text">
-                  Dec. 21 - Dec. 25, 2020
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </CardDeck>
-        </div>
-        <h2>Past Trips</h2>
-        <div className="row">
-          <CardDeck>
-            <Card className="card">
-              <Card.Img className="card-img" variant="top" src={picture} />
-              <Card.Body>
-                <Card.Title className="card-title">Yosemite</Card.Title>
-                <Card.Text className="card-text">
-                  Dec. 21 - Dec. 25, 2020
-                </Card.Text>
+                <Card.Title className="card-title">Calendar</Card.Title>
               </Card.Body>
             </Card>
             <Card className="card">
               <Card.Img className="card-img" variant="top" src={picture} />
               <Card.Body>
-                <Card.Title className="card-title">Yosemite</Card.Title>
-                <Card.Text className="card-text">
-                  Dec. 21 - Dec. 25, 2020
-                </Card.Text>
+                <Card.Title className="card-title">Packing List</Card.Title>
+              </Card.Body>
+            </Card>
+            <Card className="card">
+              <Card.Img className="card-img" variant="top" src={picture} />
+              <Card.Body>
+                <Card.Title className="card-title">Itinerary</Card.Title>
+              </Card.Body>
+            </Card>
+            <Card className="card">
+              <Card.Img className="card-img" variant="top" src={picture} />
+              <Card.Body>
+                <Card.Title className="card-title">Budget Tracker</Card.Title>
               </Card.Body>
             </Card>
           </CardDeck>
-        </div>
+        </Container>
       </div>
     )
   }
